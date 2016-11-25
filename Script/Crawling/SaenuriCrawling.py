c63d1df8254b648d90adf1101cd28cf1f8ea5b9e
ChangdongOh <lamancha91@gmail.com>
2016-11-25 17:09:17 +0900

    W2V R Code Renewal

--- Script/Crawling/SaenuriCrawling.py
+++ Script/Crawling/SaenuriCrawling.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 24 17:50:05 2016
+Created on Fri Nov 25 00:53:47 2016
 
 @author: Changdong Oh
 """
+
 
 import requests
 import re
@@ -13,27 +14,30 @@ from bs4 import BeautifulSoup
 
 def urlparser(year, page):
     headers={'Content-Type':'application/x-www-form-urlencoded',
-    'Origin':'http://theminjoo.kr',
-    'Referer':'http://theminjoo.kr/briefingList.do',
+    'Host':'www.saenuriparty.kr',
+    'Origin':'http://www.saenuriparty.kr',
+    'Referer':'http://www.saenuriparty.kr/web/news/briefing/delegateBriefing/mainDelegateBriefingView.do',
     'Upgrade-Insecure-Requests':'1',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.59 Safari/537.36'}
-    payload={'date_1':str(year),
-             'searchCondition':'0',
+    payload={'srchYear':str(year),
+             'searchCondition':'1',
+             'viewType':'L',
              'pageIndex':str(page)}
-    url='http://theminjoo.kr/briefingList.do'
+    url='http://www.saenuriparty.kr/web/news/briefing/delegateBriefing/mainDelegateBriefingView.do'
     soup=BeautifulSoup(requests.post(url, headers=headers,data=payload).text,'lxml')
-    pageurl_list= [i.find('a')['href'] for i in soup.find_all('td',class_='tal')][1:]
-    if soup.find('div',class_='paging').find('a',attrs={'onclick':"fn_egov_link_page({0}); return false;".format(str(page+1))})==None:
+    pageurl_list= ['http://www.saenuriparty.kr/web/news/briefing/delegateBriefing/readDelegateBriefingView.do?bbsId='+i.find('a')['onclick'][10:-3] for i in soup.find_all('td',class_='subject')]
+    if soup.find('div',class_='board_paging').find('a',attrs={'onclick':"_getList({0});return false; ".format(str(page+1))})==None:
         more=False
     else: more=True
     return pageurl_list, more
+    
     
 def article(url):
     url='http://theminjoo.kr'+url
     print(url)
     soup=BeautifulSoup(requests.get(url).text,'lxml')
-    date=soup.find('span',class_='txt').text
-    article=soup.find('div',class_='cont').text
+    date=soup.find('td',class_='date').find('span').text
+    article=soup.find('div',class_='editor_cont').text
     article=re.sub("(\d+)[.]\s+(\d+)[.]\s+(\d+)",r"\1년 \2월 \3일", article)
     article=re.sub('[^가-힣]+',' ',article)
     if '년 월 일' in article:
@@ -43,7 +47,7 @@ def article(url):
     
 for year in range(2008, 2017):
     import csv
-    with open('MoralFoundations\Data\민주{0}.csv'.format(str(year)),'w', encoding='UTF8', newline='') as f:
+    with open('MoralFoundations\Data\새누리{0}.csv'.format(str(year)),'w', encoding='UTF8', newline='') as f:
         w=csv.writer(f)
         more=True
         page=1
@@ -51,9 +55,7 @@ for year in range(2008, 2017):
             url_list, more=urlparser(year, page)
             for url in url_list:
                 w.writerow(article(url))
-                time.sleep(3)
+                time.sleep(1)
             page+=1
-            
-            
-        
+    
     
